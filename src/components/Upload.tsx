7fbd0151a8b768c6cab33659eaea85784a7f1ba7
itzmitto <andre.babirian2004@gmail.com>
2026-07-27 23:02:06 +0200

    styling aangepast

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,11 +1,46 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
 export default function Upload() {
+    const [image, setImage] = useState<string | null>(null);
+    const [fileName, setFileName] = useState("");
+    const [fileSize, setFileSize] = useState("");
+    function handleFile(e: ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        setFileName(file.name);
+        setFileSize((file.size / 1024 / 1024).toFixed(2) + " MB");
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+    }
     return (
         <section className="upload-section">
             <div className="upload-card">
                 <div className="upload-icon">📁</div>
                 <h2>Upload your Image or GIF</h2>
-                <p>Drag & drop your file here or click below to browse.</p>
-                <button className="upload-btn">Choose File</button>
+                <p>Select an image from your computer.</p>
+                <input
+                    type="file"
+                    accept="image/*,.gif"
+                    id="upload"
+                    hidden
+                    onChange={handleFile} />
+                <label
+                    htmlFor="upload"
+                    className="upload-btn">Choose File
+                </label>
+                {image && (
+                    <div className="preview">
+                        <img
+                            src={image}
+                            alt="Preview" />
+                        <h3>{fileName}</h3>
+                        <span>{fileSize}</span>
+                    </div>
+                )}
             </div>
         </section>
     );
