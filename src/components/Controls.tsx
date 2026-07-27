3ed5bf6bb4f92dbc7e72082b900ecf7e89f0feeb
itzmitto <andre.babirian2004@gmail.com>
2026-07-27 23:32:05 +0200

    styling verbeterd

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,39 +1,91 @@
 "use client";
+import { AsciiOptions } from "@/hooks/useAscii";
+
 type ControlsProps = {
-    width: number;
-    setWidth: (value: number) => void;
+    options: AsciiOptions;
+    setOptions: React.Dispatch<React.SetStateAction<AsciiOptions>>;
 };
-export default function Controls({ width, setWidth }: ControlsProps) {
+
+export default function Controls({ options, setOptions }: ControlsProps) {
+
+    function update<K extends keyof AsciiOptions>(key: K, value: AsciiOptions[K]) {
+        setOptions(prev => ({
+            ...prev,
+            [key]: value
+        }));
+    }
+
     return (
         <div className="controls">
             <div className="control-group">
                 <div className="control-header">
                     <span>ASCII Width</span>
-                    <span>{width}</span>
+                    <span>{options.width}px</span>
                 </div>
                 <input
                     type="range"
                     min="40"
                     max="300"
                     step="10"
-                    value={width}
-                    onChange={(e) => setWidth(Number(e.target.value))}
+                    value={options.width}
+                    onChange={(e) => update("width", Number(e.target.value))}
+                />
+            </div>
+            <div className="control-group">
+                <div className="control-header">
+                    <span>Brightness</span>
+                    <span>{options.brightness}</span>
+                </div>
+                <input
+                    type="range"
+                    min="-100"
+                    max="100"
+                    step="1"
+                    value={options.brightness}
+                    onChange={(e) => update("brightness", Number(e.target.value))}
+                />
+            </div>
+            <div className="control-group">
+                <div className="control-header">
+                    <span>Contrast</span>
+                    <span>{options.contrast}</span>
+                </div>
+                <input
+                    type="range"
+                    min="-100"
+                    max="100"
+                    step="1"
+                    value={options.contrast}
+                    onChange={(e) => update("contrast", Number(e.target.value))}
                 />
             </div>
             <div className="control-group">
                 <div className="control-header">
                     <span>Character Set</span>
                 </div>
-                <select disabled>
-                    <option>Standard (coming soon)</option>
+                <select
+                    value={options.characterSet}
+                    onChange={(e) => update("characterSet", e.target.value as AsciiOptions["characterSet"])}
+                >
+                    <option value="standard">
+                        Standard
+                    </option>
+                    <option value="dense">
+                        Dense
+                    </option>
+                    <option value="blocks">
+                        Blocks
+                    </option>
                 </select>
             </div>
-            <div className="control-group">
-                <label className="checkbox">
-                    <input type="checkbox" disabled />
-                    <span>Invert Colors (coming soon)</span>
-                </label>
-            </div>
+            <label className="checkbox">
+                <input
+                    type="checkbox"
+                    checked={options.invert}
+                    onChange={(e) => update("invert", e.target.checked)}
+                />
+                <span>Invert Colors</span>
+            </label>
         </div>
     );
 }
